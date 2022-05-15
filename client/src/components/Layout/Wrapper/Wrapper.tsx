6c6791862f87f5a97c5505ca385849a6e08229b3
artnerdnet <artnerdnet@gmail.com>
2022-05-16 00:36:38 +0200

    get products

--- client/src/components/Layout/Wrapper/Wrapper.tsx
+++ client/src/components/Layout/Wrapper/Wrapper.tsx
@@ -7,12 +7,25 @@ import PointsTag from '../../PointsTag';
 import Button from '../../Button/Button';
 import ProductCard from '../../ProductCard/ProductCard';
 
-const Wrapper: FunctionComponent = () => {
-  const { products } = useDataContext().dataContext;
 
+async function getProducts(url = '') {
+  // Default options are marked with *
+  const response = await fetch(url);
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+const Wrapper: FunctionComponent = () => {
+  const { setDataContext, dataContext } = useDataContext();
+  getProducts('http://localhost:3001/orders/user/2')
+  .then(data => {
+    // setDataContext(data.orders)
+    console.log(data.orders)
+  });
   return (
     <StyledWrapper data-testid="wrapper">
-      {products.map((product) => (
+      {dataContext?.products.map((product) => (
         <ProductCard product={product} state={product.state} />
         // <Card key={uuid()}>
         //   <StyledContentContainer>
