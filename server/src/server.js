c05328c53f54d3e69869c33eb6eaebc198bc9ac6
artnerdnet <artnerdnet@gmail.com>
2022-05-20 17:23:12 +0200

    security sanitizor

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import { productsRouter, usersRouter, ordersRouter } from './routes/index.js';
 import cors from 'cors';
+import helmet from 'helmet';
+import xssClean from 'xss-clean';
+import hpp from 'hpp';
+import { rateLimit } from 'express-rate-limit';
+// import { check } from 'express-validator'; move validation
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +13,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
+app.use(xssClean());
+app.use(hpp());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,    // 10 minutes
+  max: 100                     // 100 requests per IP
+});
+
+app.use(limiter);
 
 app.use('/', [
   usersRouter,
