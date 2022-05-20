afa00afa478b3996878cee6f38deb1182855331c
artnerdnet <artnerdnet@gmail.com>
2022-05-20 16:58:20 +0200

    images

--- client/src/components/Layout/Wrapper/Wrapper.tsx
+++ client/src/components/Layout/Wrapper/Wrapper.tsx
@@ -1,20 +1,12 @@
 import React, { FunctionComponent, useEffect } from 'react';
-import { v1 as uuid } from 'uuid';
 import { useDataContext } from '../../../global/dataContext';
-import Card from '../../Card';
-import { StyledWrapper, StyledTitle, StyledContentContainer, StyledImageWrapper, StyledImage } from './WrapperStyles';
-import PointsTag from '../../PointsTag';
-import Button from '../../Button/Button';
+import { StyledWrapper } from './WrapperStyles';
 import ProductCard from '../../ProductCard/ProductCard';
 
-
 async function getProducts(url = '') {
-  // Default options are marked with *
   const response = await fetch(url);
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.json();
 }
-
-
 
 const Wrapper: FunctionComponent = () => {
   const { setDataContext, dataContext } = useDataContext();
@@ -23,7 +15,6 @@ const Wrapper: FunctionComponent = () => {
     if (!dataContext.products.length) {
       getProducts('http://localhost:3001/orders/user/2')
       .then(data => {
-      // setDataContext(data.orders)
       setDataContext({ ...dataContext, products: data.products, loading: false })
       });
     }
@@ -34,16 +25,6 @@ const Wrapper: FunctionComponent = () => {
     <StyledWrapper data-testid="wrapper">
       {dataContext?.products.map((product) => (
         <ProductCard product={product} state={product.state} />
-        // <Card key={uuid()}>
-        //   <StyledContentContainer>
-        //       <PointsTag count={product.points} />
-        //     <StyledImageWrapper>
-        //       <StyledImage src={product.image} />
-        //     </StyledImageWrapper>
-        //     <StyledTitle>{product.name}</StyledTitle>
-        //     <Button>Redeem</Button>
-        //   </StyledContentContainer>
-        // </Card>
       ))}
     </StyledWrapper>
   );
